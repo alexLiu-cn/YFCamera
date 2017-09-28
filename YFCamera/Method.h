f44c9f681480c81317ae24d543c284127569728e
liuzhibing <373301593@qq.com>
2017-09-28 11:34:20 +0800

    first

---
+++ YFCamera/Method.h
@@ -0,0 +1,13 @@
+//
+//  Method.h
+//  TestOne
+//
+//  Created by apple on 2017/9/28.
+//  Copyright © 2017年 Bingo.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Method : NSObject
+- (void)sayHello;
+@end
---
+++ YFCamera/Method.m
@@ -0,0 +1,15 @@
+//
+//  Method.m
+//  TestOne
+//
+//  Created by apple on 2017/9/28.
+//  Copyright © 2017年 Bingo.com. All rights reserved.
+//
+
+#import "Method.h"
+
+@implementation Method
+- (void)sayHello{
+    NSLog(@"hello");
+}
+@end
